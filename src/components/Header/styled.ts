ed74035476bda98c71f88ba38cd58bebc1889fbd
runey11 <chris333meneghetti@gmail.com>
2022-03-14 22:57:45 -0300

    Correção do css do header logado

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,16 +1,31 @@
 import styled from "styled-components";
 import { corPrimaria } from "../UI/variables";
 
-export const StyledHeader = styled.nav`
+export const Main = styled.div`
+  position: relative;
+  width: 100%;
+  padding: 0 1.5rem;
   background-color: ${corPrimaria};
+`;
+
+export const StyledHeader = styled.nav`
   display: flex;
-  justify-content: space-evenly;
-  padding: 0 15vw;
+  justify-content: space-between;
+  max-width: 76rem;
+  margin: 0px auto;
+  gap: 1rem;
   height: 112px;
   align-items: center;
 `;
 
-export const Logo = styled.img`
-  height: 54px;
-  width: 158px;
+export const Logo = styled.div`
+  display: flex;
+`;
+
+export const SearchDiv = styled.div`
+  position: relative;
+  width: 100%;
+  padding: 0 1.5rem;
+  background-color: ${corPrimaria};
+  margin-right: 24px;
 `;
