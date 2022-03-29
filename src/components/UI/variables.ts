4e3f9e44c7e65f641eb51a9cfb7aaec851550096
runey11 <chris333meneghetti@gmail.com>
2022-03-29 12:35:03 -0300

    Adição do onclick nos botões de favorito e comprar, props e states

--- src/components/UI/variables.ts
+++ src/components/UI/variables.ts
@@ -3,6 +3,7 @@ export const clrWht = "#FFFFFF";
 export const clrBackground = "#011E48";
 export const clrKabum = "#FF6500";
 export const clrKabumWht = "#FAFAFB";
+export const clrKabumRed = "#E32214";
 export const clrTitleOffer = "#42464D";
 export const clrManufactor = "#7F858D";
 export const clrOfferTxt = "#565C69";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,44 @@
+import axios from "axios";
 import type { NextPage } from "next";
+import { useEffect, useReducer, useState } from "react";
 import Body from "../components/Body";
 import Header from "../components/Header";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ children }) => {
+  const [offers, setOffers] = useState([]);
+  const [favorite, setFavorite] = useState([] as any);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/offers").then((response) => {
+      setOffers(response.data);
+    });
+  }, []);
+
+  const onAddFavorite = (id: string) => {
+    setFavorite((previousFav: string[]): any => {
+      if (previousFav.includes(id)) {
+        return previousFav.filter((favedId) => favedId !== id);
+      }
+      return [...previousFav, id];
+    });
+  };
+
+  const onAddCart = (id: string) => {
+    setCart((previousAdd: string[]): any => {
+      return [...previousAdd, id];
+    });
+  };
+
   return (
     <>
-      <Header />
-      <Body />
+      <Header favoriteCount={favorite.length} cartCount={cart.length} />
+      <Body
+        ofertas={offers}
+        favorite={favorite}
+        onAddFavorite={onAddFavorite}
+        onAddCart={onAddCart}
+      />
     </>
   );
 };
